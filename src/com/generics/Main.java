0e69efc2f69b8897b964bebdee6b318a3d5ec38e
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2019-10-26 15:12:47 +0200

    new file

--- src/com/generics/Main.java
+++ src/com/generics/Main.java
@@ -4,7 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("Napis");
+        Obiekt1 o = new Obiekt1();
+
+        System.out.println("Napis "+o.dodaj(10,25));
         System.out.println("Napis1");
+
+
     }
 }
--- src/com/generics/Obiekt1.java
+++ src/com/generics/Obiekt1.java
@@ -1,4 +1,7 @@
 package com.generics;
 
 public class Obiekt1 {
+    double dodaj(double a, double b){
+        return a+b;
+    }
 }
